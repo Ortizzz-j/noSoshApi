9a423ee37149c8249b645c65eec41c0abc890064
ortiz MJ <ortizzzj99@gmail.com>
2023-06-25 22:00:37 -0700

    added to the models

---
+++ models/Thought.js
@@ -0,0 +1,61 @@
+const { Schema, Types } = require('mongoose');
+
+const reactionSchema = new Schema(
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId()
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            maxlength: 280,
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+          },
+    }
+);
+
+
+const thoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            max_length: 280,
+            min_length: 1
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now
+        },
+        username: {
+            type: Schema.Types.ObjectId,
+            ref: 'user'
+        },
+        reactions: [reactionSchema]
+    },
+    {
+        toJSON: {
+            virtuals: true
+        },
+        id: false,
+    }
+);
+
+thoughtSchema
+    .virtual('timeFormat')
+    .get(function() {
+        const formattedDate = this.createdAt.toLocaleString();
+        return formattedDate;
+    });
+
+    const Thought = model('thought', thoughtSchema);
+
+    module.exports = Thought;
