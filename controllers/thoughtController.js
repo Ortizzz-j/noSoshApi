87df80accd9d86c438e6a81fb840848c91f36f53
ortiz MJ <ortizzzj99@gmail.com>
2023-06-28 13:26:41 -0700

    update

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -26,7 +26,7 @@ module.exports = {
     async createThought(req, res) {
         try {
 
-            const thought = await Thought.create(req.body);
+            const thought = Thought.create(req.body);
 
             const user = await User.findOne({ _id: req.body.userId });
 
